10ef9d9545948f58ef59336a21e27d7a11388958
Klein <121896152+kontor-1@users.noreply.github.com>
2023-01-26 14:16:37 -0800

    bug hunting-23

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style/index.css";
 import ReactDOM from "react-dom/client";
-import { AuthContextProvider } from "./components/context/AuthContext";
+import { AuthContextProvider } from "./Components/context/AuthContext";
 import App from "./App";
 // import ReportWebVitals from "./components/utils/ReportWebVitals";
 
