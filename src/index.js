3c55c865a701f92f266c34d4a3c528abb3a55215
Klein <121896152+kontor-1@users.noreply.github.com>
2023-01-26 10:13:53 -0800

    bug hunting-14

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./style/index.css";
 import ReactDOM from "react-dom/client";
 import { AuthContextProvider } from "./components/context/AuthContext";
 import App from "./App";
-import reportWebVitals from "./components/utils/reportWebVitals";
+import ReportWebVitals from "./components/utils/ReportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,4 +17,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReportWebVitals();
